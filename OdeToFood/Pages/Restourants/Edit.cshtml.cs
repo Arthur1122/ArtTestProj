041a7f199f26b55509e8562c7d2f53e7c6b0782a
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-05-11 16:35:35 +0400

    I ceated form with method post. And select with enum for changes the data  restaurant.

--- OdeToFood/Pages/Restourants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restourants/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -12,14 +13,18 @@ namespace OdeToFood.Pages.Restourants
     public class EditModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
 
         public Restaurant Restaurant { get; set; }
-        public EditModel(IRestaurantData restaurantData)
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
             this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
         }
         public IActionResult OnGet(int restaurantId)
         {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
             Restaurant = restaurantData.GetById(restaurantId);
             if(Restaurant == null)
             {
