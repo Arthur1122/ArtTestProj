a20cf9751caa40727f2998a813de09803b2c025c
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-05-15 10:59:44 +0400

    Bulding User Interface using Layout views,  Partial views and View Components

---
+++ OdeToFood/ViewComponents/RestauranCountViewConmponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.ViewComponents
+{
+    public class RestauranCountViewConmponent : ViewComponent
+    {
+        private readonly IRestaurantData restauranData;
+
+        public RestauranCountViewConmponent(IRestaurantData restauranData)
+        {
+            this.restauranData = restauranData;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var count = restauranData.GetCountOfRestaurants();
+            return View(count);
+        }
+
+    }
+}
